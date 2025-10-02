d3ef5fbdd1fcbb2808623b89a60f265bcecce00f
GWG05 <gabrielwg2016@gmail.com>
2025-10-02 12:39:55 -0700

    Added grass type pokemon.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,5 @@
-let pokemonList = []
+let pokemonList = [
+  { name: 'Chespin', height: 0.4, types: ['grass'] },
+  { name: 'Quilladin', height: 0.7, types: ['grass'] },
+  { name: 'Chesnaught', height: 1.6,types: ['grass','fighting'] },
+]
