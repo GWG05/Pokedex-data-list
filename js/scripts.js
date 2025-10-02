df1ba62f562f0c3c15400432779f43f16cc97b2b
GWG05 <gabrielwg2016@gmail.com>
2025-10-02 12:57:54 -0700

    Added fire types to project.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
 let pokemonList = [
   { name: 'Chespin', height: 0.4, types: ['grass'] },
   { name: 'Quilladin', height: 0.7, types: ['grass'] },
-  { name: 'Chesnaught', height: 1.6,types: ['grass','fighting'] },
-]
+  { name: 'Chesnaught', height: 1.6, types: ['grass', 'fighting'] },
+  { name: 'Fennekin', height: 0.4, types: ['fire'] },
+  { name: 'Braixen', height: 1, types: ['fire'] },
+  { name: 'Delphox', height: 1.5, types: ['fire', 'psychic'] },
+];
