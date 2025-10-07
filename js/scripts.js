ee150a8679236ddc7b8e419b523590d426bd9ce3
GWG05 <gabrielwg2016@gmail.com>
2025-10-07 16:15:47 -0700

    added a for loop using my pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,16 @@ let pokemonList = [
   { name: 'Frogadier', height: 0.6, types: ['water'] },
   { name: 'Greninja', height: 1.5, types: ['water', 'dark'] }
 ];
+
+// the pokemonList for loop. (makes a list of the items it the array)
+for (let i = 0; i < pokemonList.length; i++) {
+  // makes the height values appear in the console.
+  console.log(pokemonList[i].height);
+  // makes the names and heights of the pokemon appear in the body.html
+  document.write(pokemonList[i].name + ' (height:' + pokemonList[i].height + ') ');
+
+  // if / then statement (if the height exceeds 1.5 then say 'wow, thats big' in the console.)
+  if (pokemonList[i].height > 1.5) {
+    console.log('Wow, that’s big!')
+  };
+}
