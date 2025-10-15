535d8e27d0945dc42b1f47857aeb6164e01204aa
GWG05 <gabrielwg2016@gmail.com>
2025-10-15 14:49:12 -0700

    added my foreach function (proper)

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,17 @@
 let pokemonRepository = (function () {
   let pokemonList = [
-  { name: 'Chespin', height: 0.4, types: ['grass'] },
-  { name: 'Quilladin', height: 0.7, types: ['grass'] },
-  { name: 'Chesnaught', height: 1.6, types: ['grass', 'fighting'] },
-  { name: 'Fennekin', height: 0.4, types: ['fire'] },
-  { name: 'Braixen', height: 1, types: ['fire'] },
-  { name: 'Delphox', height: 1.5, types: ['fire', 'psychic'] },
-  { name: 'Froakie', height: 0.3, types: ['water'] },
-  { name: 'Frogadier', height: 0.6, types: ['water'] },
-  { name: 'Greninja', height: 1.5, types: ['water', 'dark'] }
+    { name: "Chespin", height: 0.4, types: ["grass"] },
+    { name: "Quilladin", height: 0.7, types: ["grass"] },
+    { name: "Chesnaught", height: 1.6, types: ["grass", "fighting"] },
+    { name: "Fennekin", height: 0.4, types: ["fire"] },
+    { name: "Braixen", height: 1, types: ["fire"] },
+    { name: "Delphox", height: 1.5, types: ["fire", "psychic"] },
+    { name: "Froakie", height: 0.3, types: ["water"] },
+    { name: "Frogadier", height: 0.6, types: ["water"] },
+    { name: "Greninja", height: 1.5, types: ["water", "dark"] },
   ];
 
-  // the pokemonList for loop. (makes a list of the items it the array)
-  // for (let i = 0; i < pokemonList.length; i++) {
-  //   // makes the height values appear in the console.
-  //   console.log(pokemonList[i].height);
-  //   // makes the names and heights of the pokemon appear in the body.html
-  //   document.write(pokemonList[i].name + ' (height:' + pokemonList[i].height + ') ');
-  // }
-
-  pokemonList.forEach(function(pokemon) {
-  console.log(pokemon.name + ' (height:' + pokemon.height + ') '),
-  document.write(pokemon.name + ' (height:' + pokemon.height + ') ')
-  // if / then statement (if the height exceeds 1.5 then say 'wow, thats big' in the console.)
-  if (pokemon.height > 1.5) {
-    console.log('Wow, that\’s big!')
-    { document.write(" - Wow, that\’s big!"); } document.write("<br>");
-  };
-  });
-
-function add(pokemon) {
+  function add(pokemon) {
     pokemonList.push(pokemon);
   }
 
@@ -39,6 +21,14 @@ function add(pokemon) {
 
   return {
     add: add,
-    getAll: getAll
+    getAll: getAll,
   };
 })();
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+  document.write(pokemon.name + " (height:" + pokemon.height + ") ");
+  if (pokemon.height > 1.5) {
+    document.write(" - Wow, that’s big!");
+  }
+  document.write("<br>");
+});
