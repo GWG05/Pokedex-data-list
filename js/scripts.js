9455d3466df0c9c2d62d8f7bc759a91d1d3ca110
GWG05 <gabrielwg2016@gmail.com>
2025-11-01 15:53:25 -0700

    Installed a correct modal.

--- js/scripts.js
+++ js/scripts.js
@@ -78,57 +78,26 @@ let pokemonRepository = (function () {
   }
 
   function showModal(pokemon) {
-    let modalContainer = document.querySelector('#modal-container');
+    let modalTitle = document.querySelector(".modal-title");
+    let modalBody = document.querySelector(".modal-body");
 
-    // clear all existing modal content
-    modalContainer.innerHTML = '';
-    let modal = document.createElement('div');
-    modal.classList.add('modal');
+    modalTitle.innerText = pokemon.name;
 
-    // Add new modal content
-    let closeButtonElement = document.createElement('button');
-    // lets the close button close the modal.
-    closeButtonElement.classList.add('modal-close');
-    closeButtonElement.innerText = 'Close';
-    closeButtonElement.addEventListener('click', hideModal);
-
-    let titleElement = document.createElement('h1');
-    titleElement.innerText = 'Dex Entry';
-
-    let nameElement = document.createElement('p');
-    nameElement.innerText = pokemon.name;
-
-    let imageElement = document.createElement('img');
+    let imageElement = document.createElement("img");
     imageElement.src = pokemon.imageUrl;
 
-    let heightElement = document.createElement('p');
+    let heightElement = document.createElement("p");
     heightElement.innerText = `Height: ${pokemon.height}`;
 
-    let typesElement = document.createElement('p');
-    typesElement.innerText =`Types: ${pokemon.types
+    let typesElement = document.createElement("p");
+    typesElement.innerText = `Types: ${pokemon.types
       .map(({ type }) => type.name)
       .join(", ")}`;
-  
 
-    modal.appendChild(closeButtonElement);
-    modal.appendChild(titleElement);
-    modal.appendChild(nameElement);
-    modal.appendChild(imageElement);
-    modal.appendChild(heightElement);
-    modal.appendChild(typesElement);
-    modalContainer.appendChild(modal);
-
-    modalContainer.classList.add('is-visible');
-
-    // lets the user close the modal by clicking on the background
-    modalContainer.addEventListener('click', (e) => {
-      // Since this is also triggered when clicking INSIDE the modal
-      // We only want to close if the user clicks directly on the overlay
-      let target = e.target;
-      if (target === modalContainer) {
-        hideModal();
-      }
-    });
+    modalBody.innerHTML = "";
+    modalBody.appendChild(imageElement);
+    modalBody.appendChild(heightElement);
+    modalBody.appendChild(typesElement);
   }
 
 
